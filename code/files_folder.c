18ba27b9bb402a5eb4957307718c6a5dccfb82e9
Brayan Fajardo <brayan.f.a07@hotmail.com>
2017-09-29 18:24:31 -0600

    Nuevos cambios al comparar dp->d_name

--- code/files_folder.c
+++ code/files_folder.c
@@ -1,18 +1,11 @@
 //File that includes yhe methods related with the manipulation of directories and files
 
-
 #include <sys/types.h>
 #include <dirent.h>
 #include "stdio.h"
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-
-
-/*Read a directory
-Input: DIR pointer, returned from openDirectory
-Output: A printing 
-*/
 
 
 /*Open a directory method
@@ -23,18 +16,39 @@ struct dirent* openDirectory (char* directory){
 
 	DIR* direction;
 	struct dirent *dp;
+	char nuevaRuta[100];
 
 	if ((direction= opendir(directory)) != NULL){
 
 		while ((dp = readdir(direction)) != NULL){
 
-			printf ("%s\n", dp->d_name);	
+			if (dp->d_type == DT_DIR){
+
+				printf("Directory found 1 --> %s\n", directory);
+
+				if (!((strcmp(dp->d_name, ".")) || !(strcmp(dp->d_name,"..")) || !(strcmp(dp->d_name,"/.")))) {
+
+					printf("With . or .. --> %s\n", dp->d_name);			
+	                strcpy(nuevaRuta, directory);
+	                strcat(nuevaRuta, "/");
+	                strcat(nuevaRuta, dp->d_name);
+
+	                printf("New Route found --> %s\n", nuevaRuta);
+	            }
+
+                //openDirectory(nuevaRuta);
+            }
+            else{
+            	if (dp->d_type == DT_REG){
+					printf ("File found --> %s\n", dp->d_name);
+				}
+			}
 		}
 		closedir(direction);
 		return dp;
 	}
 	else{
-		printf("Its not possible open the directory\n");
+		printf("Its not possible open the directory\n\n");
 	}
 		return dp;
 }
@@ -43,7 +57,7 @@ struct dirent* openDirectory (char* directory){
 //Main function
 int main(int argc, char const *argv[])
 {
-	printf("It is possible run openDirectory\n");
+	printf("It is possible run openDirectory method\n\n");
 	openDirectory("/home/brayan/SOProyecto1/code");
 
 	return 0;
--- enlaces.md
+++ enlaces.md
@@ -5,3 +5,5 @@ http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
 https://stackoverflow.com/questions/23958040/checking-if-a-dir-entry-returned-by-readdir-is-a-directory-link-or-file
 
 https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
+
+https://www.gnu.org/software/libc/manual/html_node/Directory-Entries.html
